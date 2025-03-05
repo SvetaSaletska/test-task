6382e335741ae3560419fa3274e795c365592b88
SvetaSaletska <svitlanagalevich@gmail.com>
2025-03-05 15:07:44 +0200

    tablet updates

--- src/js/collections.js
+++ src/js/collections.js
@@ -2,10 +2,10 @@ import Swiper from 'swiper';
 import 'swiper/css';
 
 const swiper = new Swiper('.mySwiper', {
-    loop: true, // Зациклення слайдів
+    loop: false, // Зациклення слайдів
     centeredSlides: true, // Центруємо активний слайд
     slidesPerView: 3, // Завжди видно 3 слайди
-    spaceBetween: 26, // Відстань між слайдами
+    spaceBetween: 4, // Відстань між слайдами
     grabCursor: true, // Курсор, схожий на руку
     on: {
         slideChange: function () {
@@ -24,11 +24,11 @@ function updateSlideStyles() {
         const image = slide.querySelector('.collection-image');
         // Якщо слайд активний (по центру)
         if (slide.classList.contains('swiper-slide-active')) {
-            image.style.transform = 'scale(1.3)'; // Центральний слайд збільшується
-            image.style.opacity = '1'; // Центральний слайд чіткий
+            image.style.transform = 'scale(0.7)'; // Центральний слайд збільшується
+            image.style.opacity = '0.5'; // Центральний слайд чіткий
         } else {
-            image.style.transform = 'scale(0.7)'; // Зменшуємо бокові слайди
-            image.style.opacity = '0.5'; // Бокові зображення менш чіткі
+            image.style.transform = 'scale(1)'; // Зменшуємо бокові слайди
+            image.style.opacity = '1'; // Бокові зображення менш чіткі
         }
     });
 }
